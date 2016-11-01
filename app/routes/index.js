260226c2fc5ed05f84efac3f273e925d828c4652
jon toler <tolerjonathan@gmail.com>
2016-11-01 16:41:56 -0700

    adding new-comment button, and features

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,6 +12,11 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
+    },
+    saveComment(params) {
+    var newComment = this.store.createRecord('comment', params);
+    newComment.save();
+    this.transitionTo('index');
     }
   }
 });
