2d5dd73e75c9a3531e21c98bf68ced070ae517f1
jon toler <tolerjonathan@gmail.com>
2016-11-04 12:30:47 -0700

    adding a computed property to display comment author and content

--- app/components/comment-tile.js
+++ app/components/comment-tile.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  heading: Ember.computed('comment.author', 'comment.content', function() {
+    return this.get('comment.author') + ' - ' + this.get('comment.content');
+  }),
+
+  actions: {
+    delete(comment) {
+      if (confirm('Are you sure you want to delete this comment?')) {
+        this.sendAction('destroyComment', comment);
+      }
+    }
+  }
 });
